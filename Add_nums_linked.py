ec52a17aefbc54986e277378a6270310d8f78748
Naomi Liu <naomiliu@Naomis-MacBook-Pro.local>
2020-12-31 15:21:41 -0800

    reverse function needs work

--- Add_nums_linked.py
+++ Add_nums_linked.py
@@ -1,67 +1,47 @@
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-        self.head = None
 
-class Solution(object):
-    # def reverse(self):
-    #     self.head = self.head
-    #     self.tail = self.head #tail now points to the head
-    #     self.head = self.head.next #head now points to the value after head
-    #     current = self.head.next #current is assigned to the value after the head
-    #     while current != None: #as long as the next value is not none
-    #         self.head = current.next #keep reassigning head to the next value
-    def init(self):
+class Node:
+ 
+    # Constructor to initialize the node object
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+ 
+ 
+class LinkedList:
+ 
+    # Function to initialize head
+    def __init__(self):
         self.head = None
-
+        self.tail = None
+ 
+    # Function to reverse the linked list
     def reverse(self):
         prev = None
+        self.tail = self.head
         current = self.head
         while(current is not None):
-            next = current.next
-            current.next = prev
-            prev = current
-            current = next
+            prev = current #assign prev to current
+            current = current.next #make next value current value
         self.head = prev
+ 
+    # Utility function to print the linked LinkedList
+    def printList(self):
+        while(self.head):
+            print(self.head.data)
+            self.head = self.head.next
 
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        # added_list = []
-        # reversel1 = []
-        # reversel2 = []
-        # #reverse digits
-        # while l1.val != None:
-        #     reversel1.append(l1.val)
-        # reversel1.reverse()
-        # l1_num = int("".join(reversel1))
-            
-        # while l2.val != None:
-        #     reversel2.append(l2.val)
-        # reversel2.reverse()
-        # l2_num = int("".join(reversel2))
-            
-        # #turn digits into integer
-        # #do the same with the next linked list
-        # addition = l1_num + l2_num
-        # #add the 2 numbers
-        # str_sum = str(addition)
-        # for digit in str_sum:
-        #     self.val = digit
-        #     self = next
-        
-        # #put sum into list with one digit in each position and reverse the order
-        # return self
-llist = Solution()
-llist = ListNode(2)        
-llist.next = ListNode(4)
-llist.next.next = ListNode(3)
-l2_node = ListNode(5)
-l2_node.next = ListNode(6)
-l2_node.next.next = ListNode(4)
-Solution.reverse(llist)
-    # addTwoNumbers(l1_node, l2_node)
+    def append(self, data):
+        """Append node with data to end of list."""
+
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+
+        if self.tail is not None:
+            # Did list start as empty?
+            self.tail.next = new_node
+
+        self.tail = new_node
+
+
