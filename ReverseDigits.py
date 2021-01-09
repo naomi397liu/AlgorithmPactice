85f39d6de456f96427689cddfa317e57636f1edf
Naomi Liu <naomiliu@Naomis-MacBook-Pro.local>
2021-01-08 18:30:22 -0800

    reverses digits for 0, neg, and leading 0s

--- ReverseDigits.py
+++ ReverseDigits.py
@@ -1,6 +1,18 @@
-def reverse(self, x):
+def reverse(x):
     """
     :type x: int
     :rtype: int
     """
-    return str(x)[::-1]
+
+    if int(x) < 0 and int(x) <= (2**31-1):
+            newNum = str(x)[::-1]
+            newNum = newNum.rstrip('-')
+            newNum = newNum.lstrip('0')
+            newNum = '-' + newNum
+    elif int(x) > 0 and int(x) >= -2**31:
+        newNum = str(x)[::-1]
+        newNum = newNum.lstrip('0')
+    else:
+        newNum = 0
+    return newNum
+
