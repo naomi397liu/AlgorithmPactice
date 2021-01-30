3f5c3bd0b472d1846b011903c858f2df6bc54f65
Naomi Liu <nhliu@ucdavis.edu>
2021-01-30 23:45:51 +0000

    added left and up search directions in visit_cell()

--- count_islands.py
+++ count_islands.py
@@ -4,12 +4,16 @@ def numIslands(self, grid):
     :rtype: int
     """
     def visit_cell(row, col, grid, seen):
-            seen.add((row,col))
-            if row + 1 < len(grid) and (row+1, col) not in seen and int(grid[row+1][col]) == 1:
-                    seen.update(visit_cell(row+1, col, grid, seen))
-            if col + 1 < len(grid[0]) and(row, col+1) not in seen and int(grid[row][col+1]) == 1:
-                    seen.update(visit_cell(row, col+1, grid, seen))
-            return seen
+        seen.add((row,col))
+        if row + 1 < len(grid) and (row+1, col) not in seen and int(grid[row+1][col]) == 1:
+            seen.update(visit_cell(row+1, col, grid, seen))
+        if col + 1 < len(grid[0]) and(row, col+1) not in seen and int(grid[row][col+1]) == 1:
+            seen.update(visit_cell(row, col+1, grid, seen))
+        if row - 1 >= 0 and (row-1, col) not in seen and int(grid[row-1][col]) == 1:
+            seen.update(visit_cell(row-1, col, grid, seen))
+        if col - 1 >= 0 and (row, col-1) not in seen and int(grid[row][col-1]):
+            seen.update(visit_cell(row, col-1, grid, seen))
+        return seen
                     
                     
     #create a 'seen' and a 'to be seen':
@@ -21,6 +25,5 @@ def numIslands(self, grid):
                 seen.update(visit_cell(r, c, grid, seen))
                 print(r,c)
                 island_count += 1
-                
             
     return island_count
