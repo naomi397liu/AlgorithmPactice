fa9a15f030775a37f23fe90f6ed32f0f373a968b
Naomi Liu <nhliu@ucdavis.edu>
2021-02-02 02:27:52 +0000

    O(n) DP solutions

--- max_subarray.py
+++ max_subarray.py
@@ -18,3 +18,25 @@ def maxSubArray(self, nums):
                 maximum = num 
         maxes.append(maximum)
     return max(maxes)
+
+#     class Solution(object):
+    # def maxSubArray(self, nums):
+    #     """
+    #     :type nums: List[int]
+    #     :rtype: int
+    #     """
+    #     for i in range(len(nums)-2, -1,-1):
+    #         nums[i]=max(nums[i], nums[i]+nums[i+1])
+    #     return max(nums)
+
+#     class Solution(object):
+    # def maxSubArray(self, nums):
+    #     """
+    #     :type nums: List[int]
+    #     :rtype: int
+    #     """
+    # for i in range(1, len(nums)):
+    #         print(i, nums[i], nums[i]+nums[i-1])
+    #         nums[i]=max(nums[i], nums[i]+nums[i-1])
+            
+    #     return max(nums)
