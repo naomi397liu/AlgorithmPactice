7081d167b5b7f1b989cfa88c5b56aade70e5f8c4
Naomi Liu <nhliu@ucdavis.edu>
2021-02-08 21:19:09 +0000

    save repeats in a hash map for faster run time

---
+++ tribonacci.py
@@ -0,0 +1,18 @@
+class Solution(object):    
+    memo = {}
+    def tribonacci(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+        if n == 0:
+            return 0
+            
+        elif n == 1 or n == 2:
+            return 1
+        
+        if n not in self.memo:
+            self.memo[n] = self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
+            
+        return self.memo[n]
