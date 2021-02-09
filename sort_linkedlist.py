8338c4ad466c8d954792331aecc0cdcffc7a8b66
Naomi Liu <nhliu@ucdavis.edu>
2021-02-09 07:16:37 +0000

    merge sorted linked lists recursively

--- sort_linkedlist.py
+++ sort_linkedlist.py
@@ -27,3 +27,35 @@ def mergeTwoLists(self, l1, l2):
         currentl1 = currentl1.next
         current = current.next
     return new_head.next
+
+def mergeTwoLists(self, l1, l2):
+    """
+    :type l1: ListNode
+    :type l2: ListNode
+    :rtype: ListNode
+    """
+    #the linked lists are sorted in increasing order
+    #check to see which is bigger
+    # insert
+    def help_me(l1,l2,merged_list):
+        if l1 and l2:
+            if l1.val > l2.val:
+                merged_list.next = l2
+                l2 = l2.next
+            else:
+                merged_list.next = l1
+                l1 = l1.next
+            merged_list = merged_list.next
+            return help_me(l1,l2,merged_list)
+        elif l1:
+            merged_list.next = l1
+            return merged_list
+        elif l2:
+            merged_list.next = l2
+            return merged_list
+        
+    
+    merged_list = ListNode(0)
+    help_me(l1, l2, merged_list)
+    return merged_list.next
+        
