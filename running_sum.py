20616bac22044f03088feb0eaa6cc50c11672995
Naomi Liu <nhliu@ucdavis.edu>
2021-02-18 14:47:05 -0800

    running sum of a 1D array added

---
+++ running_sum.py
@@ -0,0 +1,9 @@
+def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+        for i in range(1,len(nums)):
+            nums[i] += nums[i-1]
+        return nums
