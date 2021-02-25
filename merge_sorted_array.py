9746b1e21c18695b3477046db9b589d29ae18a9a
Naomi Liu <nhliu@ucdavis.edu>
2021-02-24 21:40:11 -0800

    solved recursively

--- merge_sorted_array.py
+++ merge_sorted_array.py
@@ -6,14 +6,6 @@ def merge(self, nums1, m, nums2, n):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        #start from the back and insert
-        #solve it iterably then add recursion
-        # once you find a spot where an item from nums2 fits, replace an item from nums 1 and then scoot the rest down an item
-        
-        #create a copy of nums 1 by slicing at index m 
-        #iterate thru both starting from the back
-        # whoever has the greater last value, pop it off and append it to the back
-            #iterate thru og nums1 using range and rewrite in the values
     copy_nums = nums1[:m]
     
     for i in range(len(nums1)-1, -1, -1):
@@ -26,4 +18,28 @@ def merge(self, nums1, m, nums2, n):
         elif copy_nums[-1] >= nums2[-1]:
             nums1[i] = copy_nums.pop()
     return nums1
-                
+    
+def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        
+        def helper(nums1, nums2, copy_nums1, i):
+            if nums2 == [] and copy_nums1 == []:
+                return nums1
+            elif copy_nums1 == []:
+                nums1[i] = nums2.pop()
+            elif nums2 == []:
+                nums1[i] = copy_nums1.pop()
+            elif nums2[-1] > copy_nums1[-1]:
+                nums1[i] = nums2.pop()
+            elif copy_nums1[-1] >= nums2[-1]:
+                nums1[i] = copy_nums1.pop()
+            helper(nums1, nums2, copy_nums1, i-1)
+        
+        copy_nums1 = nums1[:m]
+        helper(nums1, nums2, copy_nums1, -1)
