7fae6b45b4c844a7b5f62bae3b8e7b595bf2122a
Naomi Liu <nhliu@ucdavis.edu>
2021-02-26 14:58:01 -0800

    shuffle recursive

---
+++ shuffle.py
@@ -0,0 +1,22 @@
+def shuffle(self, nums, n):
+    """
+    :type nums: List[int]
+    :type n: int
+    :rtype: List[int]
+    """
+    #will the len of yn always be the same as the len of xn? Assuming yes
+    #the first y will be paired with the first x
+    #the first y is at index n+1
+    #iterate thru nums appending x and then y to a new list until reaching n
+    
+    def helper(nums, new_nums, n):
+        if nums == []:
+            return
+        else:
+            new_nums.append(nums.pop(0))
+            new_nums.append(nums.pop(n-1))
+            helper(nums, new_nums, n-1)
+    
+    new_nums = []
+    helper(nums, [], n)
+    return new_nums
