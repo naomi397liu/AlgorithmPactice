44065a40b7840eb8f1bbc97b279f7f89ebb5ef37
Naomi Liu <nhliu@ucdavis.edu>
2021-02-25 17:25:52 -0800

    solved recursively

--- greatest_candies.py
+++ greatest_candies.py
@@ -12,4 +12,29 @@ def kidsWithCandies(self, candies, extraCandies):
             greatest_num.append(True)
         else:
             greatest_num.append(False)
+    return 
+def kidsWithCandies(self, candies, extraCandies):
+    """
+    :type candies: List[int]
+    :type extraCandies: int
+    :rtype: List[bool]
+    """
+#         how can a kid have the maximum number of candies if they take all extra candies for themselves
+#         if that kid takes all the extra candies for themselves and still dont have the greatest number of candies then output false for that index
+    
+    
+    def helper(candies, extraCandies, greatest_num, most_candy):
+        print(candies, greatest_num)
+        if candies == []:
+            return
+
+        elif candies.pop(0) + extraCandies >= most_candy:
+            greatest_num.append(True)
+        else:
+            greatest_num.append(False)
+        helper(candies, extraCandies, greatest_num, most_candy)
+        
+    greatest_num = []
+    most_candy = max(candies)
+    helper(candies, extraCandies, greatest_num, most_candy)
     return greatest_num
