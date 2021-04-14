4bdd7efebb5ee0e6a3e8b4a5d329a039b814d663
Naomi Liu <nhliu@ucdavis.edu>
2021-04-14 12:29:29 -0700

    found max number of deci-binary numbers that sum to n

---
+++ max_num_decibinary.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minPartitions(self, n):
+        """
+        :type n: str
+        :rtype: int
+        """
+
+        # for element in list(n):
+        #     int(element)
+        # return max(n)
+            
+        digits = set(n)
+        return max(digits)
