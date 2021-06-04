a5f074296894d20d3230edb3dd079785d1372c96
Naomi Liu <nhliu@ucdavis.edu>
2021-06-04 13:54:33 -0700

    slightly faster sol

--- sorting_sentence.py
+++ sorting_sentence.py
@@ -1,5 +1,8 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
+        """
+        O(2n) where the time limiting step would be the first loop
+        """
         sentence = s.split(" ")
         word_order = {}
         for word in sentence:
@@ -8,3 +11,14 @@ class Solution:
         for i in range(2,len(sentence)+1):
                 sort += " " + word_order[f"{i}"]
         return sort
+
+    def sortSentence(self, s: str) -> str:
+        """
+        O(n)
+        """
+        sentence = s.split(" ")
+        sort = [0]*len(sentence)
+        for i, word in enumerate(sentence):
+            sort[int(word[-1])-1] = word[:len(word)-1]
+        
+        return " ".join(sort)
